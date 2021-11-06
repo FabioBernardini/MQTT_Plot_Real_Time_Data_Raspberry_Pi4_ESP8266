9c7d2b381acc0c1fe3d9a527e7ee0d71d823f748
Fabio Bernardini <56018807+FabioBernardini@users.noreply.github.com>
2021-11-06 12:01:04 +0100

    Add files via upload

---
+++ MQTT_Raspberry_Subscription.py
@@ -0,0 +1,62 @@
+"""
+Author: Fabio Bernardini
+
+In this script there is the subscription to the MQTT topic by the Raspberry Pi
+to receive the sensor data coming from the ESP8266.
+The data are plotted to see their values in real-time
+
+"""
+# importiamo la libreria paho
+import paho.mqtt.client as mqtt
+import matplotlib.pyplot as plt
+import json
+
+# Creiamo le funzione di callback per il subscriber per la connessione al broker.
+
+# se il codice connessione (presente nella variabile "rc" è "0" allora la connessione è riuscita)
+def on_connect(client, userdata, flags, rc):
+    print("Connessione con il broker con codice: "+str(rc))
+
+    """
+    Effettuiamo la subscribe al topic 
+    """
+    client.subscribe("ESP8266/dataSensor")
+
+# Creiamo la funzione callback necessaria per ricevere i messaggi
+def on_message(client, userdata, msg):
+
+    # la variabile "msg" contiene il topic e il payload, cioè il contenuto del messaggio ricevuto dal publisher.
+    print("ho ricevuto un messaggio con il seguente topic: "+msg.topic+" e il seguente contenuto: "+str(msg.payload, "utf-8"))
+    
+    if msg.payload != None:
+        msg_payload = json.loads(msg.payload.decode())
+        temp = float(msg_payload["parameters"]["temp"])
+        hum = float(msg_payload["parameters"]["humidity"])
+        
+        x.append(temp)
+        y.append(hum)
+
+        fig, (ax1, ax2) = plt.subplots(2, 1)
+        ax1.plot(x,label='temp')
+        ax1.set_xlabel('Time (s)')  
+        ax1.set_ylabel('°C')  
+        ax1.title.set_text("Temperature")  
+        ax2.plot(y,label='hum')
+        ax2.set_xlabel('Time (s)')  
+        ax2.set_ylabel('%')  
+        ax2.title.set_text("Humidity")  
+        plt.show()
+
+
+# creiamo la variabile client
+client = mqtt.Client()
+
+x = list()
+y = list()
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("broker.emqx.io", 1883, 60)
+
+client.loop_forever()
